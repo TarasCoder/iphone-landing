b46098f1e350cf37bd0d223c7285cd2135990514
TarasCoder <4kaskad@gmail.com>
2022-01-21 20:02:07 +0200

    adding slow menu animation to anchors

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,6 +1,20 @@
 const links = document.querySelectorAll(".header-menu__item a");
+const linkDetail = document.querySelector(
+  ".card-details__link-characteristics"
+);
 
 seamless.polyfill();
+
+linkDetail.addEventListener("click", () => {
+    seamless.elementScrollIntoView(
+        document.querySelector("#characteristics"),
+        {
+          behavior: "smooth",
+          block: "center",
+          inline: "center",
+        }
+      );
+});
 
 links.forEach((element) => {
   element.addEventListener("click", (event) => {
