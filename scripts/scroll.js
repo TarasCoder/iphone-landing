a1f3d27457a814b57b68ee3ba2f903102cd06034
TarasCoder <4kaskad@gmail.com>
2022-01-22 23:04:17 +0200

    adding changing tabs functionality

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,42 +1,43 @@
-const links = document.querySelectorAll(".header-menu__item a");
-const linkDetail = document.querySelector(
-  ".card-details__link-characteristics"
-);
+const scrollFunc = () => {
+  const links = document.querySelectorAll(".header-menu__item a");
+  const linkDetail = document.querySelector(
+    ".card-details__link-characteristics"
+  );
 
-seamless.polyfill();
+  seamless.polyfill();
 
-linkDetail.addEventListener("click", () => {
-    seamless.elementScrollIntoView(
-        document.querySelector("#characteristics"),
-        {
-          behavior: "smooth",
-          block: "center",
-          inline: "center",
-        }
-      );
-});
-
-links.forEach((element) => {
-  element.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    const id = element.getAttribute("href").substring(1);
-    const section = document.getElementById(id);
-
-    if (section) {
-      seamless.elementScrollIntoView(section, {
-        behavior: "smooth",
-        block: "start",
-      });
-    } else {
-      seamless.elementScrollIntoView(
-        document.querySelector("#characteristics"),
-        {
-          behavior: "smooth",
-          block: "center",
-          inline: "center",
-        }
-      );
-    }
+  linkDetail.addEventListener("click", () => {
+    seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+      behavior: "smooth",
+      block: "center",
+      inline: "center",
+    });
   });
-});
+
+  links.forEach((element) => {
+    element.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      const id = element.getAttribute("href").substring(1);
+      const section = document.getElementById(id);
+
+      if (section) {
+        seamless.elementScrollIntoView(section, {
+          behavior: "smooth",
+          block: "start",
+        });
+      } else {
+        seamless.elementScrollIntoView(
+          document.querySelector("#characteristics"),
+          {
+            behavior: "smooth",
+            block: "center",
+            inline: "center",
+          }
+        );
+      }
+    });
+  });
+};
+
+scrollFunc();
